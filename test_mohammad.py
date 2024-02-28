ad26a6d9434551f792b4e86ba4fcc15a95f6c800
m.albittar <m.albittar@tu-braunschweig.de>
2024-02-28 11:26:49 +0100

    Aktaulisieren des tes_mohammad.py und kommentieren

--- test_mohammad.py
+++ test_mohammad.py
@@ -2,92 +2,75 @@ import cv2
 import numpy as np
 from collections import Counter
 
-def majority_frame(frames):
-    # Assuming all frames are of the same shape
-    h, w = frames[0].shape
-    majority_frame = np.zeros((h, w), dtype=np.uint8)
 
-    for i in range(h):
-        for j in range(w):
-            pixel_values = [frame[i, j] for frame in frames]
-            majority_value = Counter(pixel_values).most_common(1)[0][0]
-            majority_frame[i, j] = majority_value
-
-    return majority_frame
-
-# Function to process the video
-def process_video(video_path):
-    # Open the video
+# Funktion zum Erzeugen von befahrender Strecke
+def roadExtract(video_path):
+    # Erstellen von zwei Captures für Bildsubtraktion
     cap1 = cv2.VideoCapture(video_path)
     cap2 = cv2.VideoCapture(video_path)
 
 
-    # Festlegen des ersten Frames
+    # Variable zum Festlegen des ersten Frame
     first_frame = 100
 
-    # Set the starting frame
-    cap1.set(cv2.CAP_PROP_POS_FRAMES, first_frame)
-    cap2.set(cv2.CAP_PROP_POS_FRAMES, first_frame + 1)
+    # Variablen zum Justieren des Ergebnisses
+    frameDistance_var, iteration_var, threshold_var = 50, 10, 30
 
-    one_time = True
+    # Festlegen des ersten Frame
+    cap1.set(cv2.CAP_PROP_POS_FRAMES, first_frame)
+    cap2.set(cv2.CAP_PROP_POS_FRAMES, first_frame + frameDistance_var)
+
+    # Array zum Abspreichern von den Ergebnissen der Subtraktion
     cumulative_result = []
 
-    for _ in range(1000):
+    for _ in range(iteration_var):
 
-        # Read the first frame
+        # Streaming
         _, main_capture = cap1.read()
         _, next_capture = cap2.read()
 
-        # Convert the first frame to grayscale
+        # BGR2GRAY
         main_capture = cv2.cvtColor(main_capture, cv2.COLOR_BGR2GRAY)
         next_capture = cv2.cvtColor(next_capture, cv2.COLOR_BGR2GRAY)
 
-        # if one_time:
-
-        #     # Initialize an empty array for the cumulative result
-        #     cumulative_result = np.zeros_like(main_capture, dtype=np.float32)
-            
-        #     one_time = False
-
-        ## Subtract the current frame from the last frame
-        # frame_diff = cv2.absdiff(next_capture, main_capture)
-        frame_diff = next_capture - main_capture
+        # Subtraktion des cap2 vom cap1
+        frame_diff = cv2.absdiff(next_capture, main_capture)
+        # frame_diff = next_capture - main_capture
 
         # Update the cumulative result
-        _, binary_image = cv2.threshold(frame_diff, 1, 255, cv2.THRESH_BINARY)
-
-        # cumulative_result += binary_image
+        _, binary_image = cv2.threshold(frame_diff, threshold_var, 255, cv2.THRESH_BINARY)
 
         cumulative_result.append(binary_image)
 
-        # Display the current frame and the cumulative result
-        # cv2.imshow('Input Video', main_capture)
-        # cv2.imshow('Cumulative Difference', cumulative_result.astype(np.uint8))
+    # Anwenden der Funktion majority_pixel auf das resultierende Array
+    befahreneStrecken = majority_pixel(cumulative_result)
 
-        # Update the last frame
-        # last_frame = gray_frame
-
-        # print(cumulative_result)
-    
-        # Break the loop if 'q' is pressed
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-
-    # Release the video capture object and close all OpenCV windows
-    cap1.release()
-    cap2.release()
-    cv2.destroyAllWindows()
-
-    # print(cumulative_result)
-    result = majority_frame(cumulative_result)
-
-    cv2.imshow('cumu', result)
-    # Wait indefinitely until a key is pressed
+    # Display Endergebnis
+    cv2.imshow('befahrene Strecken', befahreneStrecken)
+    # Fenster schließen bei Taste
     cv2.waitKey(0)
-
-    # After a key is pressed, close the window and release resources
     cv2.destroyAllWindows()
+
+# Funktion zum Vergleich Pixel an einer bestimmten Stelle über mehrere Frames
+def majority_pixel(frames):
+    # Ablesen der Höhe und Länge des ersten Frames aus dem gegebenen Array
+    h, w = frames[0].shape
+    # Erstellen einer 2D numpy Matrix
+    majority_pixel = np.zeros((h, w), dtype=np.uint8)
+
+    # Schleife über die Zeilen eines Frame
+    for i in range(h):
+        # Schleife über die Spalten eines Frame
+        for j in range(w):
+            # Ablesen des Pixel-Wertes an der Stelle i, j und Abspeichern in der pixel_values Array für alle frames 
+            pixel_values = [frame[i, j] for frame in frames]
+            # Anwenden der Counter-Funktion aus dem Modul "collections", um das meist vorkommende Pixel herauszufinden
+            majority_value = Counter(pixel_values).most_common(1)[0][0]
+            # Abspeichern der gefundene Pixel an der Stelle i, j in der 2D Matrix majority_pixel
+            majority_pixel[i, j] = majority_value
+
+    return majority_pixel
     
 
-# Replace 'path_to_video.mp4' with the path to your video file
-process_video(r'C:\Users\moham\Downloads\DBV_WS23-24\resources\test_video.mp4')
+# Ausführen des Skript auf ein Video
+roadExtract(r'resources\test_video.mp4')
